a0e9aa0ac0e9bfdffd2e3009fb445240c695304a
Andy <108356734+MySnowLearningHub@users.noreply.github.com>
2022-06-30 10:00:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,19 +46,40 @@ except URLError as e:
   streamlit.error()
   #streamlit.write('The user entered', fruit_choice)
 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return mycur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+#my_cur = my_cnx.cursor()
+#my_data_rows = my_cur.fetchall()
+#streamlit.dataframe(my_data_rows)
+
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 #New section to display fruit from Snowflake
-streamlit.header('SNOWFLAKE fruit')
-snowflake_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding ', snowflake_fruit)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values('from "+ new_fruit +"')")
+        return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+#    streamlit.header('SNOWFLAKE fruit')
+#snowflake_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+#streamlit.write('Thanks for adding ', snowflake_fruit)
 
 #This will 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
